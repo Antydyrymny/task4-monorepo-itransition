dd584cea2f1287cd4cdee870b46b9b54124400ca
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:44:41 +0300

    testing without connection options

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -2,28 +2,24 @@ import express from 'express';
 // import passport from 'passport';
 // import { connect, disconnect } from '../database/setupConnection';
 // import { User } from '../models/user';
-// import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-// const connectionString = process.env.MONGODB_URL;
-const jwt = process.env.JWT_SECRET;
+const connectionString = process.env.MONGODB_URL;
 
 const router = express.Router();
 // router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
 router.get('/', async (req, res) => {
     try {
-        // await mongoose.connect(connectionString, {
-        //     useUnifiedTopology: true,
-        //     useNewUrlParser: true,
-        // } as ConnectOptions);
-        // mongoose.disconnect();
+        await mongoose.connect(connectionString);
+        mongoose.disconnect();
         // await connect();
         // const users = await User.find({});
         // res.status(200).json(users);
-        res.status(200).json(jwt);
+        res.status(200).json('working');
     } catch (error) {
-        // mongoose.disconnect();
+        mongoose.disconnect();
         res.status(500).json({ error });
     } finally {
         // disconnect();
