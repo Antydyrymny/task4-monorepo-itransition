c044e653ccef0b8f959e5fd4c563a92d36542b71
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-04 16:29:51 +0300

    logout route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import login from './login';
 import register from './register';
+import logout from './logout';
 import getUsers from './getUsers';
 
 const router = express.Router();
 
 router.use('/login', login);
 router.use('/register', register);
-// router.use('/logout', getUsers);
+router.use('/logout', logout);
 router.use('/users', getUsers);
 // router.use('/blockUsers', getUsers);
 // router.use('/deleteUsers', getUsers);
---
+++ backend/src/routes/logout.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import passport from 'passport';
+import dotenv from 'dotenv';
+import establishConnection from '../database/establishConnection';
+import { User } from '../models/user';
+
+dotenv.config();
+
+const router = express.Router();
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const [connect, disconnect] = establishConnection();
+    const _id: string = req.body;
+    try {
+        await connect();
+        const userToLogout = await User.findOne({ _id });
+        if (!userToLogout) {
+            res.status(404).json('User not found');
+            return;
+        }
+
+        userToLogout.status = 'offline';
+        userToLogout.save();
+
+        res.status(200).json('Loged out');
+    } catch (error) {
+        res.status(500).json({ error });
+    } finally {
+        disconnect();
+    }
+});
+
+export default router;
