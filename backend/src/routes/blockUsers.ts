c2ea5dd60429db3a257f7b0ace2820ed10e3ba93
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-04 16:43:11 +0300

    block users route

---
+++ backend/src/routes/blockUsers.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import passport from 'passport';
+import dotenv from 'dotenv';
+import establishConnection from '../database/establishConnection';
+import { User } from '../models/user';
+
+dotenv.config();
+
+const router = express.Router();
+router.patch('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const [connect, disconnect] = establishConnection();
+    const userIds: string[] = req.body;
+    try {
+        await connect();
+        const usersToBlock = await User.find({ _id: { $in: userIds } });
+        usersToBlock.forEach((user) => (user.status = 'blocked'));
+        await Promise.all(usersToBlock.map((user) => user.save()));
+
+        res.status(200).json(usersToBlock);
+    } catch (error) {
+        res.status(500).json({ error });
+    } finally {
+        disconnect();
+    }
+});
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import login from './login';
 import register from './register';
 import logout from './logout';
 import getUsers from './getUsers';
+import blockUsers from './blockUsers';
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.use('/login', login);
 router.use('/register', register);
 router.use('/logout', logout);
 router.use('/users', getUsers);
-// router.use('/blockUsers', getUsers);
+router.use('/blockUsers', blockUsers);
 // router.use('/deleteUsers', getUsers);
 
 export default router;
