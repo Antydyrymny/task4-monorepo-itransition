6672f62b68b99b587df33f886f01fee058b6f668
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:38:25 +0300

    test with importing mongoose.connect

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -1,20 +1,31 @@
 import express from 'express';
 // import passport from 'passport';
-import { connect, disconnect } from '../database/setupConnection';
+// import { connect, disconnect } from '../database/setupConnection';
 // import { User } from '../models/user';
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const connectionString = process.env.MONGODB_URL;
 
 const router = express.Router();
 // router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
 router.get('/', async (req, res) => {
     try {
-        await connect();
+        await mongoose.connect(connectionString, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        } as ConnectOptions);
+        mongoose.disconnect();
+        // await connect();
         // const users = await User.find({});
         // res.status(200).json(users);
         res.status(200).json('working');
     } catch (error) {
+        mongoose.disconnect();
         res.status(500).json({ error });
     } finally {
-        disconnect();
+        // disconnect();
     }
 });
 
