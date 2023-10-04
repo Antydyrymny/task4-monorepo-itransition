f44df98004d69be63b87c115f79e3a2c4853417b
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:35:24 +0300

    testing with connection

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -1,20 +1,20 @@
 import express from 'express';
 // import passport from 'passport';
-// import { connect, disconnect } from '../database/setupConnection';
+import { connect, disconnect } from '../database/setupConnection';
 // import { User } from '../models/user';
 
 const router = express.Router();
 // router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
 router.get('/', async (req, res) => {
     try {
-        // await connect();
+        await connect();
         // const users = await User.find({});
         // res.status(200).json(users);
         res.status(200).json('working');
     } catch (error) {
         res.status(500).json({ error });
     } finally {
-        // disconnect();
+        disconnect();
     }
 });
 
