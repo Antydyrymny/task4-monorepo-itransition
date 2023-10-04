6d1eaf2fd4426366d4b903291856d6ef92704457
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:42:59 +0300

    testing process.env

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -2,27 +2,28 @@ import express from 'express';
 // import passport from 'passport';
 // import { connect, disconnect } from '../database/setupConnection';
 // import { User } from '../models/user';
-import mongoose, { ConnectOptions } from 'mongoose';
+// import mongoose, { ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const connectionString = process.env.MONGODB_URL;
+// const connectionString = process.env.MONGODB_URL;
+const jwt = process.env.JWT_SECRET;
 
 const router = express.Router();
 // router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
 router.get('/', async (req, res) => {
     try {
-        await mongoose.connect(connectionString, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        } as ConnectOptions);
-        mongoose.disconnect();
+        // await mongoose.connect(connectionString, {
+        //     useUnifiedTopology: true,
+        //     useNewUrlParser: true,
+        // } as ConnectOptions);
+        // mongoose.disconnect();
         // await connect();
         // const users = await User.find({});
         // res.status(200).json(users);
-        res.status(200).json('working');
+        res.status(200).json(jwt);
     } catch (error) {
-        mongoose.disconnect();
+        // mongoose.disconnect();
         res.status(500).json({ error });
     } finally {
         // disconnect();
