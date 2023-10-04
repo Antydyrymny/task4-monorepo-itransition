65a9c34c42c5af0cef8792796a90f8d409eb9eb0
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:29:46 +0300

    test

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import passport from 'passport';
-import { disconnect } from '../database/setupConnection';
+// import passport from 'passport';
+import { connect, disconnect } from '../database/setupConnection';
 import { User } from '../models/user';
 
 const router = express.Router();
-router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+// router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+router.get('/', async (req, res) => {
     try {
+        await connect();
         const users = await User.find({});
         res.status(200).json(users);
     } catch (error) {
