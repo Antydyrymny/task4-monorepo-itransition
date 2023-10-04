52af57fa0842c1553e484a58676dada075b5dd20
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-05 01:48:40 +0300

    test

--- backend/src/routes/getUsers.ts
+++ backend/src/routes/getUsers.ts
@@ -16,16 +16,15 @@ router.get('/', async (req, res) => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         } as ConnectOptions);
-        mongoose.disconnect();
         // await connect();
         // const users = await User.find({});
         // res.status(200).json(users);
         res.status(200).json('working');
     } catch (error) {
-        mongoose.disconnect();
         res.status(500).json({ error });
     } finally {
         // disconnect();
+        mongoose.disconnect();
     }
 });
 
